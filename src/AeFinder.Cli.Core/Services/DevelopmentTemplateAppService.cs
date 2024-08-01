505a47f7f4ec643170db23cadd52847b61365dc3
sinclair <sinclair@aefinder.io>
2024-08-01 18:22:38 +0800

    feat: check project name

--- src/AeFinder.Cli.Core/Services/DevelopmentTemplateAppService.cs
+++ src/AeFinder.Cli.Core/Services/DevelopmentTemplateAppService.cs
@@ -1,9 +1,11 @@
 using System.Text;
+using System.Text.RegularExpressions;
 using AeFinder.Cli.Auth;
 using AeFinder.Cli.Http;
 using AeFinder.Cli.Options;
 using AeFinder.Cli.Utils;
 using Microsoft.Extensions.Logging;
+using Volo.Abp;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Http;
 using Volo.Abp.Json;
@@ -36,6 +38,8 @@ public class DevelopmentTemplateAppService : IDevelopmentTemplateAppService, ITr
     {
         _logger.LogInformation("Initialize app...");
         
+        CheckOptions(options);
+        
         var token = await _authService.GetAccessTokenAsync(options.Network, options.AppId, options.Key);
         
         var postData = _jsonSerializer.Serialize(new
@@ -63,5 +67,18 @@ public class DevelopmentTemplateAppService : IDevelopmentTemplateAppService, ITr
         ZipHelper.UnZip(result, options.Directory);
             
         _logger.LogInformation("The AeFinder App: {app} is initialized successfully. Directory: {directory}", options.Name, options.Directory);
+    }
+
+    private void CheckOptions(InitAppOptions options)
+    {
+        if (options.Name.Length is < 2 or > 20)
+        {
+            throw new UserFriendlyException("The name should be between 2 and 20 in length.");
+        }
+
+        if (!Regex.IsMatch(options.Name, "[A-Za-z][A-Za-z0-9.]+"))
+        {
+            throw new UserFriendlyException("The Name must begin with a letter and can only contain letters('A'-'Z', 'a'-'z'), numbers(0-9), and dots('.').");
+        }
     }
 }
